adf324f9f98e883523046e15d8524f801cb14182
yun.ma@ptmind.com <yun.ma@ptmind.com>
2018-09-12 00:55:18 +0800

    refactor the pagination components

--- src/components/pagination/pagination.vue
+++ src/components/pagination/pagination.vue
@@ -3,8 +3,8 @@
         <!-- <span class="m-paging__total">共{{totalPageNumber}}页</span> -->
         <ul class="m-paging__pager">
             <li class="m-paging__pager--pre" @click="jumpPage(currentPage-1)">前翻页</li>
-            <li class="m-paging__pager--number" :class="{active:currentPage===item.currentValue}" v-for="(item,index ) in pageNumberArr" :key="index" @click="jumpPage(item.currentValue)">
-                {{item.type==="direction"?"...":item.currentValue}}
+            <li class="m-paging__pager--number" :class="{active:currentPage===item.value}" v-for="(item,index ) in pageNumberArr" :key="index" @click="jumpPage(item.value)">
+                {{item.type==='ellipsis'?'...':item.value}}
             </li>
             <li class="m-paging__pager--next" @click="jumpPage(currentPage+1)">后翻页</li>
         </ul>
@@ -19,6 +19,7 @@
 
 <script>
 import "./Pagination.css";
+import { types } from "util";
 export default {
   name: "MPagination",
   props: {
@@ -31,15 +32,15 @@ export default {
       type: Number,
       default: 5
     },
-    currentPageNumber: {
+    defaultPage: {
       type: Number,
-      default: 6
+      default: 7
     }
   },
   data() {
     return {
       pageNumberArr: [],
-      currentPage: this.currentPageNumber
+      currentPage: this.defaultPage
     };
   },
   computed: {
@@ -48,136 +49,118 @@ export default {
     }
   },
   mounted: function() {
-    this.pageNumberArr = this.computePageCount();
+    this.pageNumberArr = this.computePageNumberArr();
   },
   methods: {
-    jumpPage: function(index) {
-      console.log("index", index);
-      var dataArr = processJumpData(this.pageNumberArr);
-      var _index = dataArr.indexOf(this.currentPage) + 1;
-      var _count = dataArr.length - _index - 1;
-      if (index === "-1") {
-        this.currentPage = dataArr[1] - _count;
-      } else if (index === "-2") {
-        this.currentPage = dataArr[dataArr.length - 2] + _count;
-      } else {
-        this.currentPage = index;
-      }
-    },
-    computePageCount: function() {
-      var arr = [];
-      var middleValue = Math.ceil(this.pageCount / 2);
-      var lastPageNumber = this.pageNumbers;
-      var _currentPage = this.currentPage;
-      var _pageCount = this.pageCount;
+    jumpPage: function(index) {},
+    computePageNumberArr: function() {
+      let arr = [],
+        element = {
+          index: 1, //下标
+          value: 1, //数值
+          type: "number" //type number or ellipsis
+        },
+        _currentPage = this.currentPage;
 
-      if (_currentPage > middleValue) {
-        arr[0] = {
-          currentValue: 1,
-          currentIndex: 0,
-          type: "number"
-        };
-        arr[1] = {
-          currentValue: 2,
-          currentIndex: 1,
-          type: "direction"
-        };
+      const LASTPAGE = this.pageNumbers; //last number
+      const PAGECOUNT = this.pageCount; // the count is that show pagenumbers
 
-        if (lastPageNumber - (_currentPage + _pageCount - 3) <= 1) {
-          var preNumberCount = _pageCount - 2 - (lastPageNumber - _currentPage);
-          for (let i = preNumberCount; i >= 1; i--) {
-            // arr[arr.length] = _currentPage - i;
-            let obj = {
-              currentValue: _currentPage - i,
-              currentIndex: arr.length,
-              type: "number"
-            };
-            arr.push(obj);
-          }
-          for (let i = _currentPage; i < lastPageNumber; i++) {
-            // arr[arr.length] = i;
-            let obj = {
-              currentValue: i,
-              currentIndex: arr.length,
-              type: "number"
-            };
-            arr.push(obj);
-          }
-        } else {
-          for (let i = 0; i < _pageCount - 2; i++) {
-            // arr[arr.length] = _currentPage + i;
-            let obj = {
-              currentValue: _currentPage + i,
-              currentIndex: arr.length,
-              type: "number"
-            };
-            arr.push(obj);
-          }
-          // arr[arr.length] = "-2";
-          var temp=arr[arr.length]+1;
-          arr[arr.length] = {
-            currentValue: temp,
-            currentIndex: arr.length,
-            type: "direction"
-          };
-        }
-        
-        arr[arr.length] = {
-          currentValue: lastPageNumber,
-          currentIndex: arr.length,
-          type: "number"
-        };
-      } else {
-        //first pagenumber
-        if (lastPageNumber <= _pageCount) {
-          for (let i = 0; i < lastPageNumber; i++) {
-            let obj = {
-              currentValue: i + 1,
-              currentIndex: i + 1,
-              type: "number"
-            };
-            arr.push(obj);
-          }
-        } else {
-          //always from the first pagenumber start to compute...
-          for (let i = 0; i < _pageCount - 1; i++) {
-            let obj = {
-              currentValue: i + 1,
-              currentIndex: i + 1,
-              type: "number"
-            };
-            arr.push(obj);
-          }
-          arr[arr.length] = {
-            currentValue: _pageCount,
-            currentIndex: arr.length,
-            type: "direction"
-          };
-          arr[arr.length] = {
-            currentValue: lastPageNumber,
-            currentIndex: arr.length,
+      if (LASTPAGE <= PAGECOUNT) {
+        //if last pagenum < pagecount
+        for (let i = 1; i <= LASTPAGE; i++) {
+          arr.push({
+            index: i,
+            value: i,
             type: "number"
-          };
+          });
         }
+      } else if (
+        _currentPage === 1 ||
+        _currentPage - 1 === 1 ||
+        _currentPage === LASTPAGE ||
+        _currentPage + 1 === LASTPAGE
+      ) {
+        if (_currentPage === 1 || _currentPage - 1 === 1) {
+          for (let i = 1; i <= PAGECOUNT; i++) {
+            let type = "number";
+            if (i === PAGECOUNT) {
+              type = "ellipsis";
+            }
+            arr.push({
+              index: i,
+              value: i,
+              type: type
+            });
+          }
+          arr.push({
+            index: PAGECOUNT + 1,
+            value: LASTPAGE,
+            type: "number"
+          });
+        } else if (_currentPage === LASTPAGE || _currentPage + 1 === LASTPAGE) {
+          arr.push({
+            index: 1,
+            value: 1,
+            type: "number"
+          });
+          arr.push({
+            index: 2,
+            value: 2,
+            type: "ellipsis"
+          });
+
+          for (let i = PAGECOUNT - 2; i >= 0; i--) {
+            let type = "number";
+            arr.push({
+              index: i,
+              value: LASTPAGE - i,
+              type: type
+            });
+          }
+        }
+      } else {
+        arr.push({
+          index: 0,
+          value: 1,
+          type: "number"
+        });
+        arr.push({
+          index: 1,
+          value: 2,
+          type: "ellipsis"
+        });
+        let flag = false;
+        for (let i = 0; i < PAGECOUNT - 1; i++) {
+          let type = "number";
+          let value = _currentPage + i;
+
+          if (value - 1 === LASTPAGE || value === LASTPAGE) {
+            flag = true;
+            break;
+          } else if (i + 1 === PAGECOUNT - 1) {
+            type = "ellipsis";
+          }
+          arr.push({
+            index: i + 2,
+            value: value,
+            type: type
+          });
+        }
+        arr.push({
+          index: PAGECOUNT + 1,
+          value: LASTPAGE,
+          type: "number"
+        });
       }
-      console.log(arr);
+
       return arr;
     }
   },
   watch: {
     currentPage: function() {
-      this.pageNumberArr = this.computePageCount();
+      this.pageNumberArr = this.computePageNumberArr();
     }
   }
 };
-
-function processJumpData(arr) {
-  var result = arr.filter(function(value, index, arr) {
-    if (Number.isInteger(value)) {
-      return value;
-    }
-  });
-  return result;
-}
 </script>
 
