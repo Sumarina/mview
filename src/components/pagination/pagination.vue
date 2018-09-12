c0948a9072d32ea88076e20de938e5b98079c94e
yun.ma@ptmind.com <yun.ma@ptmind.com>
2018-09-12 14:47:04 +0800

    add note

--- src/components/pagination/pagination.vue
+++ src/components/pagination/pagination.vue
@@ -3,14 +3,14 @@
         <!-- <span class="m-paging__total">共{{totalPageNumber}}页</span> -->
         <ul class="m-paging__pager">
             <!-- <li class="m-paging__pager--pre" @click="jumpPage(currentPage-1)">前翻页</li> -->
-            <li class="m-paging__pager--number" :class="{active:currentPage===item.value}" v-for="(item,index ) in pageNumberArr" :key="index" @click="jumpPage(item)">
+            <li class="m-paging__pager--number" :class="{active:currentPage===item.value}" v-for="(item,index ) in pageNumberArr" :key="index" @click="onPageTurn(item)">
                 {{item.type==='ellipsis'?'...':item.value}}
             </li>
             <!-- <li class="m-paging__pager--next" @click="jumpPage(currentPage+1)">后翻页</li> -->
         </ul>
         <span class="m-paging__jump">
         前往
-        <input class="m-paging__jump--number" type="text" v-model.number="currentPage" @blur="jumpPage(currentPage)">
+        <input class="m-paging__jump--number" type="text" v-model.number="currentPage" @input="jumpPage(currentPage)">
         页
         </span>
         
@@ -52,16 +52,27 @@ export default {
     this.pageNumberArr = this.computePageNumberArr();
   },
   methods: {
-    jumpPage: function(item) {
+    jumpPage: function(value) {
+      this.currentPage = value;
+      this.pageNumberArr = this.computePageNumberArr();
+    },
+    onPageTurn: function(item) {
       let _value = item["value"];
-      let _type = item["type"];
+      let _type = item["type"]; 
+      /**
+       * the item of current click ,it's an object that own three custom property.
+       * if the property type of this item is 'ellispsi', means this item is ellipsis.we need to get a new pageNumberArr.
+       * if the property type is 'number',means this item is number,it's needn't to get pageNumberArr.
+       */
       if (_type === "number") {
         this.currentPage = item["value"];
       } else {
         if (_value < this.currentPage) {
-          this.currentPage = this.currentPage - Math.ceil(this.pageCount / 2) - 1;
+          this.currentPage =
+            this.currentPage - Math.ceil(this.pageCount / 2) - 1;
         } else {
-          this.currentPage = this.currentPage + Math.ceil(this.pageCount / 2) + 1;
+          this.currentPage =
+            this.currentPage + Math.ceil(this.pageCount / 2) + 1;
         }
         this.pageNumberArr = this.computePageNumberArr();
       }
@@ -73,14 +84,19 @@ export default {
           value: 1, //数值
           type: "number" //type number or ellipsis
         },
-        _currentPage = this.currentPage;
+        _currentPage = this.currentPage;//current page number
 
-      const LASTPAGE = this.pageNumbers; //last number
+      const LASTPAGENUM = this.pageNumbers; //last number
       const PAGECOUNT = this.pageCount; // the count is that show pagenumbers
 
-      if (LASTPAGE <= PAGECOUNT) {
+      /**
+       * first,if last pagenumber less than pagecount ,it's means we have no need to ellipsis.
+       * second,if current page less than pagecount or more than last pagenumber minus pagecount ,it's means we only one ellipsis
+       * finally,we need two ellipsis.
+       */
+      if (LASTPAGENUM <= PAGECOUNT) {
         //if last pagenum < pagecount
-        for (let i = 1; i <= LASTPAGE; i++) {
+        for (let i = 1; i <= LASTPAGENUM; i++) {
           arr.push({
             index: i,
             value: i,
@@ -101,10 +117,10 @@ export default {
         }
         arr.push({
           index: PAGECOUNT + 1,
-          value: LASTPAGE,
+          value: LASTPAGENUM,
           type: "number"
         });
-      } else if (_currentPage > LASTPAGE - PAGECOUNT) {
+      } else if (_currentPage > LASTPAGENUM - PAGECOUNT) {
         arr.push({
           index: 1,
           value: 1,
@@ -120,7 +136,7 @@ export default {
           let type = "number";
           arr.push({
             index: i,
-            value: LASTPAGE - i,
+            value: LASTPAGENUM - i,
             type: type
           });
         }
@@ -155,19 +171,18 @@ export default {
 
         arr.push({
           index: PAGECOUNT + 1,
-          value: LASTPAGE - 1,
+          value: LASTPAGENUM - 1,
           type: "ellipsis"
         });
         arr.push({
           index: PAGECOUNT + 2,
-          value: LASTPAGE,
+          value: LASTPAGENUM,
           type: "number"
         });
       }
       return arr;
     }
   }
-  
 };
 </script>
 
