1da7369cdb2f991ae64e3ab76a728e7f7d644e5a
yun.ma@ptmind.com <yun.ma@ptmind.com>
2019-03-14 21:26:51 +0800

    add circle process component

--- examples/docs/progress.md
+++ examples/docs/progress.md
@@ -5,15 +5,29 @@
 ### 基础用法
 
 <div class="demo-block" >
-<m-progress :percent=50 :strokeWidth=20 />
-
+<m-progress :percent=50 :strokeWidth=20 :textInside="true"  />
 </div>
 
 ::: demo
 
 ```html
 <div class="demo-block" >
-<m-progress  :percent=50 :strokeWidth=20 />
+<m-progress  :percent=50 :strokeWidth=20 :textInside="true" />
+```
+
+:::
+
+<div class="demo-block" >
+<div class="demo-block" >
+<m-progress :percent=30 :strokeWidth=20 :textInside="true" :isBar=false />
+</div>
+</div>
+
+::: demo
+
+```html
+<div class="demo-block" >
+<m-progress :percent=30 :strokeWidth=20 :textInside="true" :isBar=false />
 ```
 
 :::
--- src/components/progress/progress.vue
+++ src/components/progress/progress.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="m-progress">
-        <div class="m-progress-bar">
+        <div class="m-progress-bar" v-if="isBar">
             <div class="m-progress-bar__outer" :style="barHeight">
-                <div class="m-progress-bar__inner" :style="barStyle"></div>
+                <div class="m-progress-bar__inner" :style="barStyle">
+                    <div class="m-progress-text m-progress-bar__inner--text" v-if="textInside">{{width}}</div>
+                </div>
             </div> 
         </div>
-        <div class="m-progress-text">{{width}}</div>
+        <div class="m-progress-text" v-if="!textInside">{{width}}</div>
+
+        <div class="m-progress-circle" v-if="!isBar">
+             <canvas id="circle" width="160" height="160" class="m-progress-circle__canvas"></canvas>
+        </div>
+        <div class="m-progress-text" v-if="!textInside">{{width}}</div>
     </div>
 </template>
 
@@ -21,6 +28,14 @@ export default {
         strokeWidth:{
             default:8,
             type:Number
+        },
+        textInside:{
+            default:false,
+            type:Boolean
+        },
+        isBar:{
+            default:true,
+            type:Boolean
         }
     },
     computed:{
@@ -39,6 +54,45 @@ export default {
                 'height':`${this.strokeWidth}px`,
                 'border-radius':`${this.strokeWidth/2}px`,
             }
+        }
+    },
+    mounted:function(){
+        
+        !this.isBar&&this.drawCircleProcess(this.percent);
+    },
+    methods:{
+        drawCircleProcess:function(process){
+            var c=document.getElementById("circle");
+            var context = c.getContext('2d');
+            var cWidth = c.width;
+            var cHeight = c.height;
+            
+            var circleX=cWidth/2;
+            var circleY=cHeight/2;
+            var circleR=circleX-5;
+            context.clearRect(0,0,cWidth,cHeight);
+
+            context.beginPath();
+            context.moveTo(circleX,circleY);
+            context.arc(circleX,circleY,circleR,0,Math.PI*2,false);
+            context.closePath();
+            context.fillStyle="#e5e5e5";
+            context.fill();
+
+            context.beginPath();
+            context.moveTo(circleX,circleY);
+            context.arc(circleX,circleY,circleR,-Math.PI/2,Math.PI*(2*(process/100)-0.5),false);
+            context.closePath();
+            context.fillStyle="#b2cf07";
+            context.fill();
+
+            context.beginPath();
+            context.moveTo(circleX,circleY);
+            context.arc(circleX,circleY,circleR-5,0,Math.PI*2,true);
+            context.closePath();
+            context.fillStyle="#ffffff";
+            context.fill();
+
         }
     }
 }
