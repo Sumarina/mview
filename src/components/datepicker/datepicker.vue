4fa2d11e55382694c75ce33071bf02315c65118d
sumarina <marinzoo@hotmail.com>
2019-05-30 00:00:36 +0800

    计算日期面板

--- src/components/datepicker/datepicker.vue
+++ src/components/datepicker/datepicker.vue
@@ -41,39 +41,134 @@ export default {
   name: "MDatepicker",
   data() {
     return {
-      date: "2019-05-27",
-      month: new Date().getMonth() + 1
+      month: new Date().getMonth() + 1,
+      panelData: []
     };
   },
   computed: {
     currentFullYearMonth() {
       return DATE.getFullYear() + "年" + (DATE.getMonth() + 1) + "月";
     },
-    panelData() {
-      return [
-        { week: "sunday", day: [1, 2, 3, 4, 5] },
-        { week: "monday", day: [1, 2, 3, 4, 5] },
-        { week: "tuesday", day: [1, 2, 3, 4, 5] },
-        { week: "wednesday", day: [1, 2, 3, 4, 5] },
-        { week: "thursday", day: [1, 2, 3, 4, 5] },
-        { week: "friday", day: [1, 2, 3, 4, 5] },
-        { week: "saturday", day: [1, 2, 3, 4, 5] }
-      ];
+    date() {
+      return getFullDate(DATE);
     }
+  },
+  mounted() {
+    this.panelData = [...this.computedCurrentMonthDatas(DATE)];
   },
   methods: {
     clearInput() {
       this.date = "";
       console.log("clearInput...");
-      this.computedCurrentMonthDatas();
+      this.computedCurrentMonthDatas(DATE);
     },
-    computedCurrentMonthDatas() {
-      const currentDays=DATE.getDay();
-      const totalDay=DATE.getDate();
-      console.log(currentDays,totalDay);
+    computedCurrentMonthDatas(date) {
+      const currentDate = date;
+      const currentMonth = currentDate.getMonth() + 1;
+      /**几号 */
+      const day = currentDate.getDate();
+      /**计算周几 */
+      const currentDays = currentDate.getDay();
+      /**计算当月的天数 */
+      const currentMonthDays = getMonthDays(currentMonth);
+      const lastMonthDays = getMonthDays(currentMonth - 1);
+      var datas = [];
+      for (let i = 0; i < 7; i++) {
+        /**一周7天 */
+        let days = [];
+        if (i == currentDays) {
+          let firstDay = 1;
+          for (let j = 0; j < 6; j++) {
+            /**一个面板6行 */
+            days.push(firstDay);
+            firstDay = firstDay + 7;
+            if (firstDay >= currentMonthDays) {
+              firstDay = firstDay - currentMonthDays;
+            }
+          }
+        } else if (i < currentDays) {
+          let lastDay = lastMonthDays - (currentDays - i - 1);
+          for (let j = 0; j < 6; j++) {
+            /**一个面板6行 */
+            days.push(lastDay);
+            lastDay = lastDay + 7;
+            if (j == 0 && lastDay > lastMonthDays) {
+              lastDay = lastDay - lastMonthDays;
+            } else if (j != 0 && lastDay > currentMonthDays) {
+              lastDay = lastDay - currentMonthDays;
+            }
+          }
+        } else {
+          let firstDay = 1 + i - currentDays;
+          for (let j = 0; j < 6; j++) {
+            /**一个面板6行 */
+            days.push(firstDay);
+            firstDay = firstDay + 7;
+            if (firstDay > currentMonthDays) {
+              firstDay = firstDay - currentMonthDays;
+            }
+          }
+        }
+        datas.push({
+          week: getWeekTxt(i),
+          day: days
+        });
+      }
+      
+      return datas;
     }
   }
 };
+function getWeekTxt(week) {
+  switch (week) {
+    case 0:
+      return "sunday";
+    case 1:
+      return "monday";
+    case 2:
+      return "tuesday";
+    case 3:
+      return "wednesday";
+    case 4:
+      return "thursday";
+    case 5:
+      return "friday";
+    case 6:
+      return "saturday";
+  }
+}
+function getFullDate(date) {
+  if (Object.prototype.toString.call(date).indexOf("Date") == -1) {
+    date = new Date();
+  }
+  return (
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+  );
+}
+
+function getMonthDays(month) {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    case 2: {
+      const year = new Date().getFullYear();
+      if (!year % 4 && year % 100) {
+        return 29;
+      } else return 28;
+    }
+  }
+}
 </script>
 
 <style>
